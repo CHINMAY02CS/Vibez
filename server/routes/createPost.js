cd9ab058a88f5026b12e2df5ded14ff812674d78
CHINMAY02CS <dpsvn.chin06162@gmail.com>
2025-01-13 09:58:50 +0530

    create post api completed to save post in mongodb database

--- server/routes/createPost.js
+++ server/routes/createPost.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const POST = mongoose.model("POST");
 
-router.post("/create-post", (req, res) => {
+router.post("/create-post", requireLogin, (req, res) => {
   const { title, body } = req.body;
   if (!title || !body) {
     return res.status(422).json({ error: "Please add all the fields" });
   }
-  res.json("ok");
+  req.user;
+  const post = new POST({
+    title,
+    body,
+    postedBy: req.user,
+  });
+  post
+    .save()
+    .then((result) => {
+      return res.json({ post: result });
+    })
+    .catch((error) => console.log(error));
 });
 
 module.exports = router;
