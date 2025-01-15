2be26bb8056180b31f04cab71b0f9a1b4f242b16
CHINMAY02CS <dpsvn.chin06162@gmail.com>
2025-01-15 10:10:14 +0530

    fetched and added all posts to home screen

--- vibez/src/pages/Home.tsx
+++ vibez/src/pages/Home.tsx
@@ -3,9 +3,10 @@ import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/componen
 import { Input } from "@/components/ui/input";
 import axios from "axios";
 import { Heart, Smile } from "lucide-react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [allPosts, setAllPosts] = useState([]);
   useEffect(() => {
     axios
       .get(
@@ -18,47 +19,53 @@ export default function Home() {
           },
         },
       )
-      .then((res) => console.log(res))
+      .then((res) => {
+        console.log(res);
+        setAllPosts(res.data);
+      })
       .catch((err) => console.log(err));
   }, []);
 
+  console.log(allPosts);
   return (
-    <div className="flex items-center justify-center">
-      <Card className="md:w-120">
-        <CardHeader className="p-4 border-b">
-          <CardTitle className="flex items-center gap-x-4">
-            <img
-              src="https://avatars.githubusercontent.com/u/98474924?v=4"
-              alt=""
-              className="w-8 h-8 rounded-full cursor-pointer"
-            />
-            <p className="cursor-pointer">Person Name</p>
-          </CardTitle>
-          {/* <CardDescription>Later add location here</CardDescription> */}
-        </CardHeader>
-        <CardContent className="p-0 m-0 h-80">
-          <img
-            className="h-80 w-120"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkJuhs3xtJ0V94n2PYF04yJ7JqFJ3Co7odWQ&s"
-          />
-        </CardContent>
-        <CardFooter className="flex-col items-start p-4 gap-y-2">
-          <div className="flex gap-x-2">
-            <Heart className="w-6 h-6 font-normal cursor-pointer" />
-          </div>
-          <p className="text-sm">1 Like</p>
-          This is amazing
-        </CardFooter>
-        <div className="flex items-center w-full p-4 pt-0">
-          <Smile className="w-6 h-6 mr-2 cursor-pointer" />
-          <div className="flex items-center w-full space-x-2">
-            <Input type="email" placeholder="Add your comment . . ." className="border shadow-none" />
-            <Button type="submit" className="">
-              Post
-            </Button>
-          </div>
-        </div>
-      </Card>
+    <div className="flex flex-col items-center justify-center gap-y-6">
+      {allPosts.length > 0 &&
+        allPosts.map((post, index) => {
+          return (
+            <Card className="md:w-120">
+              <CardHeader className="p-4 border-b">
+                <CardTitle className="flex items-center gap-x-4">
+                  <img
+                    src={"https://avatars.githubusercontent.com/u/98474924?v=4"}
+                    alt=""
+                    className="w-8 h-8 rounded-full cursor-pointer"
+                  />
+                  <p className="cursor-pointer">{post?.postedBy?.name}</p>
+                </CardTitle>
+                {/* <CardDescription>Later add location here</CardDescription> */}
+              </CardHeader>
+              <CardContent className="p-0 m-0 h-80">
+                <img className="h-80 w-120" src={post?.photo} />
+              </CardContent>
+              <CardFooter className="flex-col items-start p-4 gap-y-2">
+                <div className="flex gap-x-2">
+                  <Heart className="w-6 h-6 font-normal cursor-pointer" />
+                </div>
+                <p className="text-sm">1 Like</p>
+                {post?.body}
+              </CardFooter>
+              <div className="flex items-center w-full p-4 pt-0">
+                <Smile className="w-6 h-6 mr-2 cursor-pointer" />
+                <div className="flex items-center w-full space-x-2">
+                  <Input type="email" placeholder="Add your comment . . ." className="border shadow-none" />
+                  <Button type="submit" className="">
+                    Comment
+                  </Button>
+                </div>
+              </div>
+            </Card>
+          );
+        })}
     </div>
   );
 }
