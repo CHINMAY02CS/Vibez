606ee4dc99a3e0ec3e113efd5c0eb7e263198621
CHINMAY02CS <dpsvn.chin06162@gmail.com>
2025-01-15 09:46:00 +0530

    added check for token if not present then go to signin page

--- vibez/src/templates/DashboardHeader.tsx
+++ vibez/src/templates/DashboardHeader.tsx
@@ -1,6 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import VibezLogo from "@/assets/VibezLogo.png";
+import { useEffect } from "react";
 export default function DashboardHeader() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  useEffect(() => {
+    const token = localStorage.getItem("jwt");
+    if (!token) {
+      navigate("/login");
+    }
+    console.log(token);
+  }, [location.pathname]);
+
   return (
     <div className="flex items-center justify-between lg:px-8">
       <img src={VibezLogo} alt="logo" className="h-8 max-w-max" />
