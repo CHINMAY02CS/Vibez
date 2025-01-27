bb971acd8b6a1c169d62685ad0fbf24bebae6a65
CHINMAY02CS <dpsvn.chin06162@gmail.com>
2025-01-27 12:27:45 +0530

    started ui improvement

--- vibez/src/layout/Dashboard.tsx
+++ vibez/src/layout/Dashboard.tsx
@@ -7,7 +7,7 @@ export default function Dashboard() {
       <div className="fixed top-0 left-0 z-50 w-full p-4 bg-white shadow-sm h-15">
         <DashboardHeader />
       </div>
-      <div className="px-2 mt-15 md:py-4 md:px-0 bg-teal-50">
+      <div className="min-h-screen px-2 mt-15 md:py-4 md:px-0 bg-teal-50">
         <Outlet />
       </div>
     </>
