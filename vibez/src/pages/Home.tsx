f0eff0ef0d0079bb9cb85f7564d79ddc19ad15d0
CHINMAY02CS <dpsvn.chin06162@gmail.com>
2025-01-28 11:04:27 +0530

    moved home apis to functions

--- vibez/src/pages/Home.tsx
+++ vibez/src/pages/Home.tsx
@@ -71,18 +71,16 @@ export default function Home() {
     fetchPosts();
   }, []);
 
-  function navigateToPage(to: string) {
-    navigate(`/user/${to}`);
-  }
-
   const userId = JSON.parse(localStorage.getItem("user") ?? "")._id;
 
   return (
     <div className="flex flex-col items-center justify-center gap-y-6">
       {allPosts.length > 0 &&
         allPosts.map((post) => {
-          const postOwner = post?.postedBy;
           const postId = post?._id;
+          const postOwner = post?.postedBy;
+          const isLiked = post?.likes?.includes(userId) || false;
+          const filteredComments = post?.comments?.filter((comment: CommentDetails) => comment.comment);
           return (
             <Card className="md:w-120" key={post._id}>
               {/* User pic and name */}
@@ -98,25 +96,22 @@ export default function Home() {
                   </p>
                 </CardTitle>
               </CardHeader>
-              {/* Post Content */}
+              {/* Post Card */}
               <CardContent className="p-0 m-0 h-80">
                 <img className="h-80 w-120" src={post?.photo} alt="postedPic" />
               </CardContent>
               <CardFooter className="flex-col items-start p-4 gap-y-2">
-                <div className="flex gap-x-1">
-                  <LikeButton
-                    isLiked={post?.likes?.includes(userId) || false}
-                    onClick={() => (post?.likes?.includes(userId) ? unlikePost(postId) : likePost(postId))}
-                  />
-                  <p className="mt-1.5 text-xs">{post?.likes?.length || "0"} Likes</p>
-                </div>
+                <LikeButton
+                  totalLikes={post?.likes?.length || "0"}
+                  showLabel
+                  isLiked={isLiked}
+                  onClick={() => (isLiked ? unlikePost(postId) : likePost(postId))}
+                />
                 {post?.body}
-
+                {/* Post details dialog */}
                 <AlertDialog>
                   <AlertDialogTrigger className="text-xs text-gray-500 hover:underline">
-                    {post.comments?.length > 0
-                      ? `View all ${post.comments.filter((comment: CommentDetails) => comment.comment).length} comments`
-                      : null}
+                    {post.comments?.length > 0 ? `View all ${filteredComments.length} comments` : null}
                   </AlertDialogTrigger>
                   <AlertDialogContent className="px-3 py-0 rounded-lg max-h-176 lg:max-w-240 lg:max-h-none">
                     <div className="flex items-end justify-end">
@@ -124,53 +119,52 @@ export default function Home() {
                         <X className="w-4 h-4" />{" "}
                       </AlertDialogCancel>
                     </div>
+                    <div className="flex items-center p-2 -mt-6 border border-gray-200 rounded-sm gap-x-4">
+                      <UserIconPic
+                        showImage={postOwner?.Photo ? true : false}
+                        src={post.postedBy.Photo || ""}
+                        path={postOwner?._id}
+                      />
+                      <p className="cursor-pointer" onClick={() => navigate(`/user/${postOwner?._id}`)}>
+                        {postOwner?.name}
+                      </p>
+                    </div>
                     <div className="grid items-start lg:grid-cols-2 gap-x-4">
                       <img className="h-80 w-120" src={post?.photo} />
                       {/* right card */}
                       <div>
-                        <div className="flex items-center p-2 border border-gray-200 rounded-sm gap-x-4">
-                          <UserIconPic
-                            showImage={postOwner?.Photo ? true : false}
-                            src={post.postedBy.Photo || ""}
-                            path={postOwner?._id}
-                          />
-                          <p className="cursor-pointer" onClick={() => navigateToPage(`${postOwner?._id}`)}>
-                            {postOwner?.name}
-                          </p>
-                        </div>
-                        <div className="pb-2 mt-2 overflow-y-auto border border-gray-100 max-h-52 lg:max-h-76">
+                        <div className="pb-2 mt-2 overflow-y-auto border border-gray-100 lg:mt-0 max-h-52 lg:max-h-76">
                           {post.comments.length > 0 &&
-                            post.comments
-                              .filter((comment: CommentDetails) => comment.comment)
-                              .map((comment: CommentDetails, index) => {
-                                const commentOwner = comment?.postedBy;
-                                return (
-                                  <div className="flex items-center p-2 gap-x-4" key={index}>
-                                    <UserIconPic
-                                      showImage={commentOwner?.Photo ? true : false}
-                                      src={comment.postedBy.Photo || ""}
-                                      path={commentOwner?._id}
-                                    />
-                                    <div>
-                                      <p
-                                        className="text-sm font-bold cursor-pointer"
-                                        onClick={() => navigateToPage(`${commentOwner?._id}`)}
-                                      >
-                                        {commentOwner?.name}
-                                      </p>
-                                      <p className="text-xs cursor-pointer">{comment.comment}</p>
-                                    </div>
+                            filteredComments.map((comment: CommentDetails, index) => {
+                              const commentOwner = comment?.postedBy;
+                              return (
+                                <div className="flex items-center p-2 gap-x-4" key={index}>
+                                  <UserIconPic
+                                    showImage={commentOwner?.Photo ? true : false}
+                                    src={comment.postedBy.Photo || ""}
+                                    path={commentOwner?._id}
+                                  />
+                                  <div>
+                                    <p
+                                      className="text-sm font-bold cursor-pointer"
+                                      onClick={() => navigate(`/user/${commentOwner?._id}`)}
+                                    >
+                                      {commentOwner?.name}
+                                    </p>
+                                    <p className="text-xs cursor-pointer">{comment.comment}</p>
                                   </div>
-                                );
-                              })}
+                                </div>
+                              );
+                            })}
                         </div>
                         <div className="flex items-center w-full p-3 px-1 gap-x-4">
                           <div className="flex items-center gap-x-1">
                             <LikeButton
-                              isLiked={post?.likes?.includes(userId) || false}
-                              onClick={() => (post?.likes?.includes(userId) ? unlikePost(postId) : likePost(postId))}
+                              totalLikes={post?.likes?.length || "0"}
+                              isLiked={isLiked}
+                              onClick={() => (isLiked ? unlikePost(postId) : likePost(postId))}
                             />
-                            <p className="mt-1.5 text-xs">{post?.likes ? post.likes.length : "0"}</p>
+                            {/* <p className="mt-1.5 text-xs">{post?.likes ? post.likes.length : "0"}</p> */}
                           </div>
                           <div className="flex items-center w-full">
                             <Smile className="w-4 h-4 mr-2 cursor-pointer" />
@@ -225,12 +219,27 @@ const UserIconPic = ({ showImage, src, path }: { showImage: boolean; src: string
   );
 };
 
-const LikeButton = ({ isLiked, onClick }: { isLiked: boolean; onClick: () => void }) => (
+const LikeButton = ({
+  isLiked,
+  onClick,
+  totalLikes,
+  showLabel,
+}: {
+  isLiked: boolean;
+  showLabel?: boolean;
+  onClick: () => void;
+  totalLikes: string | number;
+}) => (
   <div className="flex items-center gap-x-1">
     <Heart
       className={`w-6 h-6 font-normal cursor-pointer ${isLiked ? "text-red-600 fill-red-600" : ""}`}
       onClick={onClick}
     />
+    {totalLikes && (
+      <p className="mt-1.5 text-xs">
+        {totalLikes || "0"} {showLabel && "Likes"}
+      </p>
+    )}
   </div>
 );
 
